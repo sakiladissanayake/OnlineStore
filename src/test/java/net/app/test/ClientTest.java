0bb5183e590595c576f2633043451dd3679c13a6
sakiladissanayake <sakilad@gmail.com>
2020-01-26 19:28:13 +0530

    Update junit version

--- src/test/java/net/app/test/ClientTest.java
+++ src/test/java/net/app/test/ClientTest.java
@@ -10,7 +10,7 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import junit.framework.Assert;
 import net.app.rest.domain.Item;
